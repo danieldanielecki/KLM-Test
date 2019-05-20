a3b06a7151ade55566cf0c91351b053902d7c23b
Daniel Danielecki <daniel.danielecki@mail.com>
2019-05-20 22:03:16 +0200

    initial commit

---
+++ apps/workspace-klm/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: "workspace-klm",
+  preset: "../../jest.config.js",
+  coverageDirectory: "../../coverage/apps/workspace-klm/",
+  snapshotSerializers: [
+    "jest-preset-angular/AngularSnapshotSerializer.js",
+    "jest-preset-angular/HTMLCommentSerializer.js"
+  ]
+};
