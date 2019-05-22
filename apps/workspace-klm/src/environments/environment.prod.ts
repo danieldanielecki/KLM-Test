87903b08eed4302ba1b3cda936e0a7f2bb4f46f0
Daniel Danielecki <daniel.danielecki@mail.com>
2019-05-22 22:30:25 +0200

    Add: Firebase with CI integration

--- apps/workspace-klm/src/environments/environment.prod.ts
+++ apps/workspace-klm/src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
+declare const API_KEY_FIREBASE_PRODUCTION: string;
+
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: API_KEY_FIREBASE_PRODUCTION,
+    authDomain: "klm-test-prod.firebaseapp.com",
+    databaseURL: "https://klm-test-prod.firebaseio.com",
+    projectId: "klm-test-prod",
+    storageBucket: "klm-test-prod.appspot.com",
+    messagingSenderId: "909361874450",
+    appId: "1:909361874450:web:35352039ef01b80d"
+  }
 };
--- apps/workspace-klm/src/environments/environment.ts
+++ apps/workspace-klm/src/environments/environment.ts
@@ -1,9 +1,19 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
+declare const API_KEY_FIREBASE_DEVELOPMENT: string;
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: API_KEY_FIREBASE_DEVELOPMENT,
+    authDomain: "klm-test-dev.firebaseapp.com",
+    databaseURL: "https://klm-test-dev.firebaseio.com",
+    projectId: "klm-test-dev",
+    storageBucket: "klm-test-dev.appspot.com",
+    messagingSenderId: "637039707351",
+    appId: "1:637039707351:web:f33ec705a16b5135"
+  }
 };
 
 /*
