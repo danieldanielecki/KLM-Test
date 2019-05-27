77de7b2401727660beda808fdfd3d3b4e0304ac3
Daniel Danielecki <daniel.danielecki@mail.com>
2019-05-27 18:29:25 +0200

    Fix: E2E tests with Agastya

--- apps/workspace-klm-e2e/src/integration/app.spec.ts
+++ apps/workspace-klm-e2e/src/integration/app.spec.ts
@@ -4,14 +4,16 @@ describe('Page: home', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display welcome message', () => {
+    cy.wait(2000); // Required in order to pass the test due to the Agastya XHR request.
     getGreeting().contains('Departures from Amsterdam Schiphol');
   });
 });
 
-// describe('Page: not found', () => {
-//   beforeEach(() => cy.visit('/not-found'));
+describe('Page: not found', () => {
+  beforeEach(() => cy.visit('/not-found'));
 
-//   it('should display page not found message', () => {
-//     getGreeting().contains('Page Not Found');
-//   });
-// });
+  it('should display page not found message', () => {
+    cy.wait(2000); // Required in order to pass the test due to the Agastya XHR request.
+    getGreeting().contains('Page Not Found');
+  });
+});
