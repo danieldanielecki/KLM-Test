7ed05115dd36cdf3dfaaa4d102048d3578adc6f5
Daniel Danielecki <daniel.danielecki@mail.com>
2019-05-28 21:05:35 +0200

    Add: PWA

--- apps/workspace-klm/src/app/app.module.ts
+++ apps/workspace-klm/src/app/app.module.ts
@@ -3,10 +3,12 @@ declare const API_KEY_AGASTYA: string;
 import Agastya from 'agastya';
 import { AppComponent } from './app.component';
 import { CoreModule } from '@libs/core/src/index';
+import { environment } from '../environments/environment';
 import { HomeModule } from '@libs/home/src/index';
 import { NgModule } from '@angular/core';
 import { NxModule } from '@nrwl/nx';
 import { RoutingModule } from './routing/app-routing.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
 import { SharedModule } from '@libs/shared/src/index';
 
 const agastya = new Agastya(API_KEY_AGASTYA);
@@ -19,6 +21,9 @@ const agastya = new Agastya(API_KEY_AGASTYA);
     HomeModule,
     NxModule.forRoot(),
     RoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production
+    }),
     SharedModule
   ]
 })
